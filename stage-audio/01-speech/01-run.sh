10284b83836a14f3daec0fc8322797801fce6899
builderjer <builderjer@gmail.com>
2023-08-25 10:01:19 -0600

    fix wget downloads

--- stage-audio/01-speech/01-run.sh
+++ stage-audio/01-speech/01-run.sh
@@ -20,7 +20,7 @@ install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share"
 install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share/piper_tts"
 install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share/piper_tts/voice-en-gb-alan-low"
 
-wget https://github.com/rhasspy/piper/releases/download/v0.0.2/voice-en-gb-alan-low.tar.gz -P "${ROOTFS_DIR}/home/ovos/.local/share/piper_tts/voice-en-gb-alan-low/voice-en-gb-alan-low.tar.gz"
+wget https://github.com/rhasspy/piper/releases/download/v0.0.2/voice-en-gb-alan-low.tar.gz -P "${ROOTFS_DIR}/home/ovos/.local/share/piper_tts/voice-en-gb-alan-low/"
 tar -xvzf "${ROOTFS_DIR}/home/ovos/.local/share/piper_tts/voice-en-gb-alan-low/voice-en-gb-alan-low.tar.gz"
 
 install -v -m 0644 files/ovos-audio.service "${ROOTFS_DIR}/etc/systemd/user/ovos-audio.service"
