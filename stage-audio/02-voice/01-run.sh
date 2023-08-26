c551e6a3d3305075ac960627bd19f0fca6fd0a48
builderjer <builderjer@gmail.com>
2023-08-26 08:21:30 -0600

    fixes - ww download - tts downloads...I hope

--- stage-audio/02-voice/01-run.sh
+++ stage-audio/02-voice/01-run.sh
@@ -5,9 +5,10 @@ install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share"
 install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share/openwakeword"
 install -v -d -m 0755 "${ROOTFS_DIR}/home/ovos/.local/share/precise_lite"
 
-wget https://github.com/dscripka/openWakeWord/blob/main/openwakeword/resources/models/hey_mycroft_v0.1.onnx -P "${ROOTFS_DIR}/home/ovos/.local/share/openwakeword/"
+wget https://github.com/dscripka/openWakeWord/raw/main/openwakeword/resources/models/hey_mycroft_v0.1.tflite -P "${ROOTFS_DIR}/home/ovos/.local/share/openwakeword/"
 
-wget https://github.com/OpenVoiceOS/precise-lite-models/blob/master/wakewords/en/hey_mycroft.tflite -P "${ROOTFS_DIR}/home/ovos/.local/share/precise_lite/"
+
+wget https://github.com/OpenVoiceOS/precise-lite-models/raw/master/wakewords/en/hey_mycroft.tflite -P "${ROOTFS_DIR}/home/ovos/.local/share/precise_lite/"
 
 install -v -m 0644 files/ovos-dinkum-listener.service "${ROOTFS_DIR}/etc/systemd/user/ovos-dinkum-listener.service"
 install -v -m 0755 files/ovos-systemd-dinkum-listener "${ROOTFS_DIR}/usr/libexec/ovos-systemd-dinkum-listener"
