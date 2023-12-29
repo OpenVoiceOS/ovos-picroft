8d9b34b164f1abfc710b238298edbf9528716114
Bohdan Buinich <bohdan.buinich@gmail.com>
2023-12-29 14:57:41 +0200

    install ovos-docs-viewer with pip

--- stage-prep/05-user/02-run-chroot.sh
+++ stage-prep/05-user/02-run-chroot.sh
@@ -3,12 +3,8 @@ python -m venv /home/ovos/.venv
 source /home/ovos/.venv/bin/activate
 
 pip3 install -U pip
-pip3 install wheel
+pip3 install wheel textual
 
-git clone https://github.com/OpenVoiceOS/ovos-docs-viewer
-mv ovos-docs-viewer/ovos_docs_viewer/ovos_docs.py /home/ovos/.local/bin/ovos_docs.py
-rm -rf ovos-docs-viewer
-
-pip3 install textual
+pip3 install git+https://github.com/OpenVoiceOS/ovos-docs-viewer
 
 deactivate
