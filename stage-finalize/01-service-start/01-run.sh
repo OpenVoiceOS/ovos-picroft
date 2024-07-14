6aefd1036065d339ba1b02521799d2c410f7bff1
builderjer <builderjer@gmail.com>
2024-07-14 08:09:57 -0600

    minor fix with PHAL moved ovos-admin-phal to user service added  to home directory of image

--- stage-finalize/01-service-start/01-run.sh
+++ stage-finalize/01-service-start/01-run.sh
@@ -1,11 +1,3 @@
 #/bin/bash -e
 
-# rm "${ROOTFS_DIR}/usr/libexec/init_resize"
-# install -v -m 0755 files/init_resize "${ROOTFS_DIR}/usr/libexec/init_resize"
-# install -v -m 0755 files/resize_fs.sh "${ROOTFS_DIR}/usr/libexec/resize_fs.sh"
-# install -v -m 0644 files/resize_fs.service "${ROOTFS_DIR}/etc/systemd/system/resize_fs.service"
-#
-# install -v -d -m 0755 "${ROOTFS_DIR}/opt/ovos"
-# touch "${ROOTFS_DIR}/opt/ovos/resize_fs"
-#
-# echo "enable resize_fs.service" >> "${ROOTFS_DIR}/etc/systemd/system-preset/10-ovos-system.preset"
+install -v -m 0755 "${WORK_DIR}/update.sh" "${ROOTFS_DIR}/home/ovos/update.sh"
--- stage-phal/01-user/01-run.sh
+++ stage-phal/01-user/01-run.sh
@@ -3,5 +3,9 @@
 install -v -m 0755 files/ovos-systemd-phal "${ROOTFS_DIR}/usr/libexec/ovos-systemd-phal"
 install -v -m 0644 files/ovos-phal.service "${ROOTFS_DIR}/etc/systemd/user/ovos-phal.service"
 
+install -v -m 0755 files/ovos-systemd-admin-phal "${ROOTFS_DIR}/usr/libexec/ovos-systemd-admin-phal"
+install -v -m 0644 files/ovos-admin-phal.service "${ROOTFS_DIR}/etc/systemd/user/ovos-admin-phal.service"
+
+echo "enable ovos-admin-phal.service" >> "${ROOTFS_DIR}/etc/systemd/user-preset/10-ovos-user.preset"
 echo "enable ovos-phal.service" >> "${ROOTFS_DIR}/etc/systemd/user-preset/10-ovos-user.preset"
 
